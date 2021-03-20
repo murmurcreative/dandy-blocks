98dec8db3cd298433ad10a9088b729915d381553
Troy Whiteley <24781+dawnerd@users.noreply.github.com>
2021-03-20 00:27:06 +0000

    WIP header block

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
-# ft-acf-blocks
+# Dandy Blocks
 A collection of Gutenberg blocks built on top of ACF.
+
+## Registering a new block
+
+### config.json
+
+Blocks need to be added to the `config.json` file.
+
+Inside of the `config.json` there is a `blocks` object. Object keys are the block slugs used for loading the template. The values of the object should match the options available to [acf_register_block_type](https://www.advancedcustomfields.com/resources/acf_register_block_type/).
+
+The basic structure should look like:
+
+```json
+{
+  "blocks": {
+    "blockSlug": {
+      //... block options
+    }
+  }
+}
+```
+
+Some block options are automatically configured and some can be overridden. Look at the `acf_register_block_type` section of `dandy-blocks.php` to see how the block is setup in code.
+
+### Templates
+
+Place the block template in the `blocks/<blockSlug>/block.php` file. You may place css/js in the `blocks/<blockSlug>` directory/
+
+Templates should use default clientkit classnames when possible and included styles should expect to be processed by CK (meaning you can use color variables and the like).
+
+### Styles & Javascript
+
+No styles or javascript are loaded by default. It is up to the theme to import the styles and javascript. If you add styles or js to a block, make sure it gets imported to the root `styles.css` or `scripts.js`
+
+Block styles should be prefixed with `dandy-block__` to make sure there's no conflicts with existing blocks WordPress might have or other plugins register.
---
+++ blocks/header/block.php
@@ -0,0 +1,51 @@
+<?php
+
+/**
+ * Header block
+ *
+ * @param   array $block The block settings and attributes.
+ * @param   string $content The block inner HTML (empty).
+ * @param   bool $is_preview True during AJAX preview.
+ * @param   (int|string) $post_id The post ID this block is saved to.
+ */
+
+// Create id attribute allowing for custom "anchor" value.
+$id = 'dandy-header-' . $block['id'];
+if (!empty($block['anchor'])) {
+  $id = $block['anchor'];
+}
+
+// Create class attribute allowing for custom "className"
+$class_name = 'dandy-block__header';
+if (!empty($block['className'])) {
+  $class_name .= ' ' . $block['className'];
+}
+
+$text = get_field('text') ?: 'Some Header';
+$subtitle = get_field('subtitle') ?: null;
+$heading_level = get_field('heading_level') ?: 'h2';
+$header_link = get_field('header_link') ?: null;
+?>
+
+<section id="<?= esc_attr($id); ?>" class="<?= esc_attr($class_name); ?>">
+  <?php if (!empty($header_link)): ?>
+  <a href="<?= esc_url($header_link); ?>">
+  <?php endif; ?>
+    <<?= esc_attr($heading_level); ?> class="dandy-block__heade__title">
+      <?= esc_html($text); ?>
+      <?php if (!empty($subtitle)): ?>
+      <span class="sr-only"><?= esc_html($subtitle); ?></span>
+      <?php endif; ?>
+    </<?= esc_attr($heading_level); ?>>
+  <?php if (!empty($header_link)): ?>
+  </a>
+  <?php endif; ?>
+
+  <?php if (!empty($subtitle)) : ?>
+  <p class="dandy-block__header__subtitle display-flex flex-space-between-center margin-top-sm" aria-hidden="true">
+    <span class="margin-xaxis-xs font-small text-stretched text-uppercase font-secondary">
+      <?= esc_html($subtitle); ?>
+    </span>
+  </p>
+  <?php endif; ?>
+</section>
